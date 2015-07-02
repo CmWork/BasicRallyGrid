777f0ee1864e999dfc80c5df5ae2e039e23a79cd
cmorikuni <c@m.com>
2015-07-02 11:47:50 -1000

    Retro Text

--- App.js
+++ App.js
@@ -8,6 +8,13 @@ Ext.define('CustomApp', {
             type: 'hbox',
             align: 'stretch'
         }
+    },{
+        xtype: 'container',
+        itemId: 'text-container',
+        layout: {
+            type: 'hbox',
+            align: 'stretch'
+        }
     }],
     launch: function() {
         console.log('Basic Rally Grid');
@@ -21,83 +28,87 @@ Ext.define('CustomApp', {
             labelAlign: 'right',
             width: '300',
             listeners: {
-                ready: this._loadFieldValueCb,
+                ready: this._loadData,
                 select: this._loadData,
                 scope: this
             }
         };
         this.down('#pulldown-container').add(iterPicker);
     },
-    _loadFieldValueCb: function() {
-        var fieldValueCb = {
-            xtype: 'rallyfieldvaluecombobox',
-            itemId: 'field-value',
-            fieldLabel: 'Owner',
-            labelAlign: 'right',
-            model: 'User Story',
-            field: 'Owner',
-            listeners: {
-                ready: this._loadData,
-                select: this._loadData,
-                scope: this
-            }
-        };
-        this.down('#pulldown-container').add(fieldValueCb);
-    },
-    _getFilters: function(iterVal, ownerVal) {
+    _getFilters: function(iterVal) {
         var iterationFilter = Ext.create('Rally.data.wsapi.Filter', {
             property: 'Iteration',
             operation: '=',
             value: iterVal
         });
-        var ownerFilter = Ext.create('Rally.data.wsapi.Filter', {
-            property: 'Owner',
-            operation: '=',
-            value: ownerVal
-        });
-        return iterationFilter.and(ownerFilter);
+        return iterationFilter;
     },
     _loadData: function() {
         var iterRef = this.down('#iter-picker').getRecord().get('_ref');
-        var owner = this.down('#field-value').getRecord().get('value');
-        console.log('SELECT OWNER: ', owner);
         console.log('iter ref: ', iterRef);
 
-        var comboFilter = this._getFilters(iterRef, owner);
+        var comboFilter = this._getFilters(iterRef);
 
         if (this.myStore) {
             this.myStore.setFilter(comboFilter);
             this.myStore.load();
         } else {
-            this.myStore = Ext.create('Rally.data.wsapi.Store', {
-                model: 'User Story',
+            this.myStore = Ext.create('Rally.data.wsapi.artifact.Store', {
+                models: ['User Story', 'Defect'],
                 autoLoad: true,
                 filters: comboFilter,
                 listeners: {
                     load: function(store, data, success) {
-                        console.log('store: ', store);
-                        if (!this.mygrid) {
-                            this._createGrid(store);
+                        var htmlStr = this._getArtifactHtml(store.getRecords());
+                        console.log(htmlStr)
+                        if (!this.mytext) {
+                            this._createText(htmlStr);
                         }
+                        this._setText(htmlStr)
                     },
                     scope: this
                 },
-                fetch: ['FormattedID', 'Name', 'Owner', 'ScheduleState']
+                fetch: ['FormattedID', 'Name', 'Owner', 'ScheduleState', 'PlanEstimate', 'RevisionHistory']
             });
             console.log('myStore: ', this.myStore);
         }
     },
-    _createGrid: function(store) {
-        this.mygrid = {
-            xtype: 'rallygrid',
-            store: store,
-            columnCfgs: [
-                'FormattedID',
-                'Name',
-                'Owner'
-            ]
+    _createText: function(string) {
+        this.mytext = {
+            itemId: 'texted',
+            xtype: 'rallyrichtexteditor',
+            height: '400',
+            width: '500',
+            value: string
         };
-        console.log('my grid: ', this.mygrid);
-        this.add(this.mygrid);
+        this.down('#text-container').add(this.mytext);
+    },
+    _setText: function(string) {
+        this.down('#texted').setValue(string)
+    },
+    _getArtifactHtml: function(records) {
+        var htmlStr = '';
+        Ext.Array.each(records, function(rec) {
+            recStr = rec.get('FormattedID') + ': ' + rec.get('Name')
+            pEst = rec.get('PlanEstimate')
+            if (pEst && pEst > 0) {
+                recStr = recStr + ' (' + pEst + 'pts)'
+            }
+            schState = rec.get('ScheduleState');
+            if (schState == 'Accepted' || schState == 'Completed') {
+                htmlStr = htmlStr + '<font color="green">' + recStr + '</font><BR>'
+            } else if (schState == 'Incomplete') {
+                htmlStr = htmlStr + '<font color="red">' + recStr + '</font><BR>'
+            } else {
+                htmlStr = htmlStr + recStr + '<BR>'
+            }
+            console.log(rec.get('FormattedID'));
+            console.log(rec.get('Name'));
+            console.log(rec.get('Owner'));
+            console.log(rec.get('ScheduleState'));
+            console.log(rec.get('PlanEstimate'));
+            console.log(rec.get('RevisionHistory'));
+        });
+        return htmlStr;
     }
 });
